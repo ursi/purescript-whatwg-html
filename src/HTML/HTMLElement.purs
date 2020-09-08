5eadf03865b8c77a919c9b2c87ade1926c853a92
Mason Mackaman <masondeanm@aol.com>
2020-09-08 02:21:51 -0400

    Add Web IDL text to HTMLElement

--- src/HTML/HTMLElement.purs
+++ src/HTML/HTMLElement.purs
@@ -43,3 +43,38 @@ instance nonDocumentTypeChildNode :: IsNonDocumentTypeChildNode HTMLElement
 instance parentNode :: IsParentNode HTMLElement
 
 instance slottable :: IsSlottable HTMLElement
+
+{-
+interface HTMLElement : Element {
+  [HTMLConstructor] constructor();
+
+  // metadata attributes
+  [CEReactions] attribute DOMString title;
+  [CEReactions] attribute DOMString lang;
+  [CEReactions] attribute boolean translate;
+  [CEReactions] attribute DOMString dir;
+
+  // user interaction
+  [CEReactions] attribute boolean hidden;
+  undefined click();
+  [CEReactions] attribute DOMString accessKey;
+  readonly attribute DOMString accessKeyLabel;
+  [CEReactions] attribute boolean draggable;
+  [CEReactions] attribute boolean spellcheck;
+  [CEReactions] attribute DOMString autocapitalize;
+
+  [CEReactions] attribute [LegacyNullToEmptyString] DOMString innerText;
+
+  ElementInternals attachInternals();
+};
+
+HTMLElement includes GlobalEventHandlers;
+HTMLElement includes DocumentAndElementEventHandlers;
+HTMLElement includes ElementContentEditable;
+HTMLElement includes HTMLOrSVGElement;
+
+[Exposed=Window]
+interface HTMLUnknownElement : HTMLElement {
+  // Note: intentionally no [HTMLConstructor]
+};
+-}
