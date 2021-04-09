b020cd05e0dd7b926abbe987c119b7d7fc685f8c
Mason Mackaman <masondeanm@aol.com>
2021-04-09 14:43:19 -0400

    Update package.nix

--- package.nix
+++ package.nix
@@ -1,8 +1,6 @@
 { ps-pkgs, ps-pkgs-ns, ... }:
   with ps-pkgs;
-  { repo = "https://github.com/ursi/purescript-whatwg-html.git";
-    rev = "f787a33c656f58f9b24b8a847a63c6b3cf15bc09";
-    dependencies =
+  { dependencies =
       let inherit (ps-pkgs-ns) ursi; in
       [ nullable
         ursi.prelude
