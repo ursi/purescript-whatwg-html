ac9cff3fe094799d5dc3bc122470b2bbe9140f0a
Mason Mackaman <masondeanm@aol.com>
2021-06-19 00:23:27 -0400

    Add activeElement

--- src/WHATWG/HTML/Mixin/DocumentOrShadowRoot.purs
+++ src/WHATWG/HTML/Mixin/DocumentOrShadowRoot.purs
@@ -1,7 +1,18 @@
-module WHATWG.HTML.Mixin.DocumentOrShadowRoot where
+module WHATWG.HTML.Mixin.DocumentOrShadowRoot (activeElement) where
 
-{-
-partial interface mixin DocumentOrShadowRoot {
-  readonly attribute Element? activeElement;
-};
--}
+import MasonPrelude
+
+import Data.Nullable (Nullable)
+import Data.Nullable as Nullable
+import WHATWG.HTML.Types (class DocumentOrShadowRoot, HTMLElement)
+import WHATWG.Internal as I
+
+-- readonly attribute Element? activeElement;
+activeElementNullable :: ∀ d.
+  DocumentOrShadowRoot d
+  => d
+  -> Effect (Nullable HTMLElement)
+activeElementNullable = I.unsafeGet "activeElement"
+
+activeElement :: ∀ d. DocumentOrShadowRoot d => d -> Effect (Maybe HTMLElement)
+activeElement = map Nullable.toMaybe <. activeElementNullable
